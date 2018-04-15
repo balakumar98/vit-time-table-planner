94a1f308f164c7822f95bec267a1b4aa23ba5cf2
balakumar98 <balashanmugam2016@gmail.com>
2018-04-15 12:05:54 +0530

    tt

---
+++ index.php
@@ -0,0 +1,85 @@
+<html>
+<head>
+<title>FFCS Planner</title>
+</head>
+<br><b><center><p><span style="font-size: 60px ; ">T</span><span style="font-size: 30px ; ">IME </span><span style="font-size: 63px">T</span><span style="font-size: 30px ; ">ABL</span><span style="font-size: 60px ; ">E</span></center><br></p></center></b>
+<p align="right"><font face="arial" color="#5f6a89">- an efficient ffcs planner </font></p>
+
+
+<?php include('backup/table.html'); ?>
+
+
+<br><br><br><br><center><b><font color="#291B2C" style="font-size: 32px">MORNING HOURS</font></b><br><br><br>
+
+<?php include('backup/t1.html'); ?>
+<?php include('backup/a1.html'); ?>
+<?php include('backup/b1.html'); ?>
+<?php include('backup/c1.html'); ?>
+<?php include('backup/d1.html'); ?>
+<?php include('backup/e1.html'); ?>
+<?php include('backup/f1.html'); ?>
+<?php include('backup/g1.html'); ?>
+<?php include('backup/ta1.html'); ?>
+<?php include('backup/tc1.html'); ?>
+<?php include('backup/te1.html'); ?>
+<?php include('backup/tg1.html'); ?>
+<?php include('backup/tcc1.html'); ?>
+<?php include('backup/taa1.html'); ?>
+<?php include('backup/tdd1.html'); ?>
+<?php include('backup/la1.html'); ?>
+<?php include('backup/la3.html'); ?>
+<?php include('backup/la5.html'); ?>
+<?php include('backup/la7.html'); ?>
+<?php include('backup/la9.html'); ?>
+<?php include('backup/la11.html'); ?>
+<?php include('backup/la13.html'); ?>
+<?php include('backup/la15.html'); ?>
+<?php include('backup/la19.html'); ?>
+<?php include('backup/la21.html'); ?>
+<?php include('backup/la23.html'); ?>
+<?php include('backup/la25.html'); ?>
+<?php include('backup/la27.html'); ?>
+<?php include('backup/la29.html'); ?>
+
+
+
+<body>
+<br><br><center><b><font color="#291B2C" style="font-size: 32px">EVENING HOURS</font></b></center><br>
+
+<?php include('backup/t2.html'); ?>
+<?php include('backup/a2.html'); ?>
+<?php include('backup/b2.html'); ?>
+<?php include('backup/c2.html'); ?>
+<?php include('backup/d2.html'); ?>
+<?php include('backup/e2.html'); ?>
+<?php include('backup/f2.html'); ?>
+<?php include('backup/g2.html'); ?>
+<?php include('backup/ta2.html'); ?>
+<?php include('backup/taa2.html'); ?>
+<?php include('backup/tbb2.html'); ?>
+<?php include('backup/tcc2.html'); ?>
+<?php include('backup/tdd2.html'); ?>
+<?php include('backup/tc2.html'); ?>
+<?php include('backup/te2.html'); ?>
+<?php include('backup/tg2.html'); ?>
+<?php include('backup/la31.html'); ?>
+<?php include('backup/la33.html'); ?>
+<?php include('backup/la35.html'); ?>
+<?php include('backup/la37.html'); ?>
+<?php include('backup/la39.html'); ?>
+<?php include('backup/la41.html'); ?>
+<?php include('backup/la43.html'); ?>
+<?php include('backup/la45.html'); ?>
+<?php include('backup/la47.html'); ?>
+<?php include('backup/la49.html'); ?>
+<?php include('backup/la51.html'); ?>
+<?php include('backup/la53.html'); ?>
+<?php include('backup/la55.html'); ?>
+<?php include('backup/la57.html'); ?>
+<?php include('backup/la59.html'); ?>
+
+</center>
+<br><br><br>
+<p align="right" >-by B<font color="#cca969">A</font>L<font color="#cca969">A</font> <font color="#cca969">K</font>U<font color="#cca969">M</font>A<font color="#cca969">R</font> S</p>
+</body>
+</html> 
